f3633dddf0eb29275cf6787630f18ccdb558318b
pueblowebs <pueblowebs@gmail.com>
2026-04-10 14:01:37 -0300

    chore: remove pepe content-type

--- backend/src/api/pepe/controllers/pepe.ts
+++
@@ -1,7 +0,0 @@
-/**
- * pepe controller
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreController('api::pepe.pepe');
--- backend/src/api/pepe/services/pepe.ts
+++
@@ -1,7 +0,0 @@
-/**
- * pepe service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::pepe.pepe');
