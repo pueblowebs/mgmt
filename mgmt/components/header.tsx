82c16ce3cf3e0cbc70e889275552923814260ec4
pueblowebs <pueblowebs@gmail.com>
2026-07-16 23:16:01 -0300

    disable FAQ access

--- mgmt/components/header.tsx
+++ mgmt/components/header.tsx
@@ -27,7 +27,7 @@ export function Header() {
     { label: "PRO", href: "/pro" },
     { label: "Nosotros", href: "/nosotros" },
     // { label: "Blog", href: "/blog" },
-    { label: "FAQ", href: "/faq" },
+    // { label: "FAQ", href: "/faq" },
     { label: "Contacto", href: "/contacto" },
   ]
 
